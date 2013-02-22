d3d8c9ec627c86a9cc9c1fd4a066a7a38b136920
Eleazar Meza <meza.eleazar@gmail.com>
2013-02-22 16:21:51 -0430

    Fixed UpdatecontentIdContentTypeInTransactions migration

--- db/migrate/20130205201804_update_content_id_content_type_in_transactions.rb
+++ db/migrate/20130205201804_update_content_id_content_type_in_transactions.rb
@@ -1,12 +1,14 @@
 class UpdateContentIdContentTypeInTransactions < ActiveRecord::Migration
   def self.up
     Transaction.all.each do |t|
-      unless t.warehouse.nil?
-        puts t.id
-        t.content_id = t.warehouse.content_id
-        t.content_type = t.warehouse.warehouse_type_id
-        t.save
+      warehouse_query = "SELECT * FROM warehouses WHERE id = #{t.warehouse_id} LIMIT 1"
+      warehouse = ActiveRecord::Base.connection.select_one(warehouse_query)
+      if warehouse.nil?
+        next
       end
+      t.content_id = warehouse['content_id']
+      t.content_type = warehouse['warehouse_type_id']
+      t.save
     end
   end
 
