5d669f2e852721532ef12581b48cf0eb115b7156
Eleazar Meza <meza.eleazar@gmail.com>
2013-11-02 11:47:20 -0430

    Updated alarm validations syntax

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,5 +1,5 @@
 class Alarm < ActiveRecord::Base
   belongs_to :order
   belongs_to :alarm_type
-  validates_presence_of :order_id, :description, :date, :alarm_type
+  validates :order_id, :description, :date, :alarm_type, presence: true
 end
