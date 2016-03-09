a0894e9324bb66f6c3a405ac53d92a8b95059ea7
Eleazar Meza <bairesdev_emeza@iseatz.com>
2016-03-09 16:12:16 -0430

    Fix truck license plate and driver ci uniqueness validation scope

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,7 +2,7 @@ class Driver < ActiveRecord::Base
   has_many :tickets
 
   validates :name, :ci, presence: true
-  validates :ci, uniqueness: {if: :frequent, case_sensitive: false}
+  validates :ci, uniqueness: {if: :frequent, case_sensitive: false, scope: :frequent}
 
   validates :ci, length: {within: 3..10}
   validates :name, length: {within: 3..40}
