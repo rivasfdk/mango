c807c5b3ec79f06b94a96597f758ca9027ad9b3f
rivasfdk <rivasfdk@gmail.com>
2017-06-11 20:29:54 -0400

    fix a medicament recipes edit and delete actions

--- app/models/medicament_recipe.rb
+++ app/models/medicament_recipe.rb
@@ -8,6 +8,8 @@ class MedicamentRecipe < ActiveRecord::Base
   validates_presence_of :name, :code
   validates_length_of :name, :within => 3..40
 
+  accepts_nested_attributes_for :ingredient_medicament_recipe, allow_destroy: true, reject_if: lambda { |ir| t[:ingredient_id].blank? }
+
   def is_associated?()
     Order.where(:medicament_recipe_id => self.id).any?
   end
@@ -15,4 +17,9 @@ class MedicamentRecipe < ActiveRecord::Base
   def get_total
     ingredient_medicament_recipe.pluck(:amount).reduce { |total, amount| total + amount }
   end
+
+  def to_collection_select
+    return "#{self.name} - #{self.code}"
+  end
+
 end
